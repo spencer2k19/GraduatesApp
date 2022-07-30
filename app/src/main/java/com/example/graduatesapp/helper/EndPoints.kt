3d36ca3ed88572411f9e4d23c8c52f1029a374bb
spencer2k19 <hachemeloic@gmail.com>
2022-07-30 09:31:01 +0200

    Ngrok funnel

--- app/src/main/java/com/example/graduatesapp/helper/EndPoints.kt
+++ app/src/main/java/com/example/graduatesapp/helper/EndPoints.kt
@@ -3,6 +3,7 @@ package com.example.graduatesapp.helper
 class EndPoints {
     companion object {
         private const val HOST_URL = "http://192.168.43.136/diplomas/public"
+//        private const val HOST_URL = "https://228d-41-138-89-249.ngrok.io/diplomas/public"
         const val BASE_URL = "$HOST_URL/api/"
         const val BASE_URL_FILES = "$HOST_URL/images/graduates"
         const val BASE_URL_AVATAR = "$HOST_URL/avatars"
